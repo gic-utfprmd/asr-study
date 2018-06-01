0ceb4b8f126953427171394f199fbad179446e4b
dresson <edresson1@gmail.com>
2018-06-01 12:50:49 -0300

    update

--- utils/hparams.py
+++ utils/hparams.py
@@ -52,15 +52,54 @@ class HParams(object):
             return self.update(values)
 
         if type(values) in (set, list):
-            tmp = {}
-            for k, v in zip(values[::2], values[1::2]):
-                try:
-                    tmp[k] = ast.literal_eval(v)
-                except ValueError:
-                    tmp[k] = v
-            return self.update(tmp)
-
-        return self.update(ast.literal_eval(values))
+            try:# python2 and python3 support working 
+                tmp = {}
+                
+                if values[0].find('=') ==  -1:# accept values ex: "num_features 10  num_classes 20"
+                    
+                    split = values[0].strip().split(' ')
+                    
+                    for i in range(0,len(split),2):
+                        try:
+                            tmp[split[i]] = int(split[i+1])
+                        except:
+                            tmp[split[i]] = float(split[i+1])
+                            
+                    return tmp
+                else:# accept values ex: " num_features=10  num_classes=20 "
+                    tmp = {}
+                    split = values[0].strip().split('=')
+                    aux = []
+                    for i in range(len(split)):
+                        lsplit  = split[i].split(' ')
+                        for i in lsplit:
+                            if i != '':
+                                aux.append(i)
+                    
+                    for i in range(0,len(aux),2):
+                        try:
+                            tmp[aux[i]] = int(aux[i+1])#for int argument 
+                        except:
+                            tmp[aux[i]] = float(aux[i+1])#for float argument
+                            
+                        
+                    return self.update(tmp)
+        
+            except:#python2 maintained original support
+                
+                tmp = {}
+                
+                for k, v in zip(values[::2], values[1::2]):
+                    
+                    try:
+                        
+                        tmp[k] = ast.literal_eval(v)
+                    except ValueError:
+                        tmp[k] = v
+                
+                return self.update(tmp)
+        
+            return self.update(ast.literal_eval(values))
 
     def values(self):
         """Return the hyperparameter values as a Python dictionary."""
